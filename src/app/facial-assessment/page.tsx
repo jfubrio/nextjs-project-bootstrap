31a98433b3826c1f98e093d31f202e9a99e3cc31
jfubrio <jfubrio@gmail.com>
2025-08-27 09:22:43 -0600

    Add src/app/facial-assessment/page.tsx 1756308162720

---
+++ src/app/facial-assessment/page.tsx
@@ -0,0 +1,9 @@
+import FacialAssessmentFormWorking from '@/components/FacialAssessmentFormWorking';
+
+export default function FacialAssessmentPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <FacialAssessmentFormWorking />
+    </div>
+  );
+}
